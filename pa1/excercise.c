c771fe0c011e6c4ea23e843a0015d231e2b25bf5
junsung0713 <49336230+junsung0713@users.noreply.github.com>
2022-04-06 13:37:46 +0900

    add: add pa 1

---
+++ pa1/excercise.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    flaot beerAvg;
+    
+    float excerciseAvg;
+    return 0;
+}
---
+++ pa1/leap.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define weightInc 6.80389
+#define year 365
+#define beerCal 150
+
+int main(void){
+    float beerAvg;
+    int priceAvg;
+    int leapYear; 
+    float totalDrunk;
+    float totalCal;
+    float totalWeightInc;
+    float totalPrice;
+    // float weightInc;
+
+    printf("1일 평균 맥주 소비 캔 수 : ");
+    scanf("%f",&beerAvg);
+    printf("평균 맥주 가격 : ");
+    scanf("%d",&priceAvg);
+    printf("윤년인지 아닌지 선택하시오 (윤년 = 1, 평년 = 0) : ");
+    scanf("%d",&leapYear);
+    totalDrunk = beerAvg * (year + leapYear);
+    totalCal = beerAvg * beerCal * (year + leapYear);
+    totalWeightInc = beerAvg * weightInc;
+    totalPrice = priceAvg * beerAvg * (year + leapYear);
+    // printf("%f",beerAvg);
+    // printf("%d",priceAvg);
+    printf("총 맥주 소비 캔: %.2f \n",totalDrunk);
+    printf("총 맥주 칼로리량: %.2fcals\n", totalCal);
+    printf("몸무게 증가량: %.2fkg \n", totalWeightInc);
+    printf("총 맥주 소비 지출 액: %.0f원 \n", totalPrice);
+    return 0 ;
+}
+
